eb991b5482b34a1d869396919ab432185107a14e
Kurikichi <n79.katayama.yusuke@gmail.com>
2020-07-17 18:27:49 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_17_051638) do
+ActiveRecord::Schema.define(version: 2020_07_17_075630) do
 
   create_table "cals", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "title"
+    t.string "title", limit: 255
     t.text "content"
     t.datetime "start_time"
     t.datetime "created_at", null: false
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 2020_07_17_051638) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
